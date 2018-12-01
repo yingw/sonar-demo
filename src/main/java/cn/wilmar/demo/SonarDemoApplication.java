fdc8b90956d9bee1bf74e73332a82dfc052744b3
Yin Guo Wei <yinguowei@gmail.com>
2018-12-01 09:34:50 +0800

    init

---
+++ src/main/java/cn/wilmar/demo/SonarDemoApplication.java
@@ -0,0 +1,12 @@
+package cn.wilmar.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarDemoApplication.class, args);
+    }
+}

bd79b96acf56157b1773fa6a31d45c990b8a6a6b
Yin Guo Wei <yinguowei@cn.wilmar-intl.com>
2018-12-01 11:18:49 +0800

    add jacoco support.

--- src/main/java/cn/wilmar/demo/SonarDemoApplication.java
+++ src/main/java/cn/wilmar/demo/SonarDemoApplication.java
@@ -2,11 +2,23 @@ package cn.wilmar.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author yinguowei
+ */
+@RestController
 @SpringBootApplication
 public class SonarDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SonarDemoApplication.class, args);
     }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+
 }
--- src/test/java/cn/wilmar/demo/SonarDemoApplicationTests.java
+++ src/test/java/cn/wilmar/demo/SonarDemoApplicationTests.java
@@ -1,16 +1,41 @@
 package cn.wilmar.demo;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SonarDemoApplicationTests {
 
+    @Autowired WebApplicationContext context;
+    @Autowired SonarDemoApplication sonarDemoApplication;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
     @Test
-    public void contextLoads() {
+    public void hello() throws Exception {
+        Assert.assertEquals("Hello, World!", sonarDemoApplication.hello());
+        mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testMain() {
+        SonarDemoApplication.main(new String[]{});
     }
 
 }
